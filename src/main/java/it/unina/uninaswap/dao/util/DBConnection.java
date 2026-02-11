5e9b8cd2da587427e1d1fd5cac4f8e4e0dae8f4b
Raz3r <mario.buffardi1@gmail.com>
2026-02-12 00:03:53 +0100

    Update DBConnection.java

--- src/main/java/it/unina/uninaswap/dao/util/DBConnection.java
+++ src/main/java/it/unina/uninaswap/dao/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 	
 	private static final String URL = "jdbc:postgresql://localhost:5432/UninaSwap";
 	private static final String USER = "postgres";
-	private static final String PASSWORD = "59556";
+	private static final String PASSWORD = "";
 	
 	public static Connection getConnection() throws SQLException {
 		return DriverManager.getConnection(URL, USER, PASSWORD);
